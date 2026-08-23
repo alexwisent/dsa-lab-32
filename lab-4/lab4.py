7069ea3b2deb92f6524fa3532270956caf9384ea
Софья Анчугова <anchugova.sonya@gmail.com>
2026-08-23 20:51:42 +0700

    создан файл index.html

--- lab-4/lab4.py
+++ lab-4/lab4.py
@@ -15,18 +15,19 @@ login_manager = LoginManager()  # менеджер авторизации
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
-# 2. Модель User
+# 2. Создать модель для работы с пользователями User на основе базового класса UserMixin 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     name = db.Column(db.String(1000), nullable=False)
 
-    def __repr__(self):
+    def __repr__(self): # вывод email
         return f'<User {self.email}>'
 
 
-# Обязательная функция для flask-login
+# Функция load_user обязательна для flask-login, чтобы при каждом запросе по сохранённому 
+# в сессии id находить пользователя в базе и делать его доступным через current_user.
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -35,4 +36,13 @@ def load_user(user_id):
 # Создание таблицы
 with app.app_context():
     db.create_all()
+
+
+# 3. Создать endpoint для перехода на корневую страницу GET /. 
+@app.route('/')
+def index():
+    if current_user.is_authenticated:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
 
