fdc59328b8d5a437e39b4a7b6a2ab33471e55e0f
Софья Анчугова <anchugova.sonya@gmail.com>
2026-08-24 16:31:13 +0700

    6. Создать endpoint для перехода на страницу регистрации GET /signup.

--- lab-4/lab4.py
+++ lab-4/lab4.py
@@ -82,5 +82,11 @@ def login():
     return render_template('login.html')
 
 
+# 6. Создать endpoint для перехода на страницу регистрации GET /signup. 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
