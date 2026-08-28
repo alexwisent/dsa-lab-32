419cf8ae6066cef19a07115462eccba72d1de902
Софья Анчугова <anchugova.sonya@gmail.com>
2026-08-28 13:25:39 +0700

    комментарии

--- lab-5/triangle_class.py
+++ lab-5/triangle_class.py
@@ -8,9 +8,9 @@ class Triangle:
         self.b = b
         self.c = c
 
-    def triangle_type(self):
+    def triangle_type(self):    # передает сохраненые стороны в get_triangle_type, которая проверят и возвращает тип треугольника
         return get_triangle_type(self.a, self.b, self.c)
 
-    def perimeter(self):
+    def perimeter(self):    # вычсиляет и возращает перимерт 
         return self.a + self.b + self.c
     
